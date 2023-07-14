c58c4e3d471e2ce97c1c9040318690fb9da9d21f
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-07-14 17:42:18 +0200

    made Compose compile

--- example/android/app/src/main/kotlin/live/theo/sdk/flutter_theolive_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/live/theo/sdk/flutter_theolive_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package live.theo.sdk.flutter_theolive_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
